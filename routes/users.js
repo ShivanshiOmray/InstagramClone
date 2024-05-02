7ad4123da3a595f9cc16721b06c5f78ba1ae3c4d
ShivanshiOmray <shiviomray@gmail.com>
2024-05-02 13:27:33 +0530

    Login and Register page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,59 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/instagram");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  bio: String,
+  profileImage: {
+    type: String,
+    default: "default.jpg",
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+  ],
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  followings: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  story: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "story",
+    },
+  ],
+  stories: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "story",
+    },
+  ],
+  messages: {
+    type: Array,
+    default: [],
+  },
+  saved: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+  ],
 });
 
-module.exports = router;
+mongoose.plugin(plm);
+
+module.exports = mongoose.model("user", userSchema);
